192aa814de0dccce5195e03c1d011decccac4f02
tuagkcuf <jokersms222@gmail.com>
2023-04-24 18:57:47 +0300

    add writetoanycontract

---
+++ contracts/WriteToAnySlot.sol
@@ -0,0 +1,36 @@
+// Solidity storage is like an array of length 2^256. Each slot in the array can store 32 bytes.
+// Order of declaration and the type of state variables define which slots it will use.
+// However, using assembly, you can write to any slot
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+
+library StorageSlot {
+    // wrap address in a struct so that it can be passed around as a storage pointer
+    struct AddressSlot {
+        address value;
+    }
+
+    function getAddressSlot(
+        bytes32 slot
+    ) internal pure returns (AddressSlot storage pointer) {
+        assembly {
+            // get the pointer to AddressSlot stored at slot
+            pointer.slot := slot
+        }
+    }
+}
+
+contract TestSlot {
+    bytes32 public constant TEST_SLOT = keccak256("TEST_SLOT");
+
+    function write(address _addr) external {
+        StorageSlot.AddressSlot storage data = StorageSlot.getAddressSlot(TEST_SLOT);
+        data.value = _addr;
+    }
+
+    function get() external view returns (address) {
+        StorageSlot.AddressSlot storage data = StorageSlot.getAddressSlot(TEST_SLOT);
+        return data.value;
+    }
+}
